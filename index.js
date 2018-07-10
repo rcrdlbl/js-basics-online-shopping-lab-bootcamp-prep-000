34f13284322162c4ed7fd130e269796da7de64b1
Przemyslaw Dabrowski <bluh@outlook.com>
2018-07-10 00:27:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,8 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
     if (cart[i]['itemName'] === item) {
-      
+      cart.splice(i, 1);
+      return cart;
     } else {
       return "That item is not in your cart."
     }
